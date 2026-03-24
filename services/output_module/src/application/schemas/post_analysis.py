3ef65803fdbaaffaf8859c4aea9c9ea171977222
Nastyasolnyyt <nastyasolntsevasunny@gmail.com>
2026-03-24 05:41:44 +0300

    fixed post_analysis

--- services/output_module/src/application/schemas/post_analysis.py
+++ services/output_module/src/application/schemas/post_analysis.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from src.application.schemas.topic import TopicDTO
 
 class PostAnalysisDTO(BaseModel):
@@ -7,19 +7,21 @@ class PostAnalysisDTO(BaseModel):
     id: int
     post_id: int
     topic_id: Optional[int] = None
-    emotion: float
-    tonality: float
-    relevance: float
-    sentiment_label: Optional[str] = None # Добавил из твоей БД
+    emotion: Optional[float] = 0.0
+    tonality: Optional[float] = 0.0
+    relevance: Optional[float] = 0.0
+    sentiment_label: Optional[str] = None
 
 class PostAnalysisWithExternalModelsDTO(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: Optional[int] = None
     post_id: int
     topic: Optional[TopicDTO] = None
-    emotion: float
-    tonality: float
-    relevance: float
-    sentiment_label: Optional[str] = None # Заменяем category на то, что есть в БД
-    # Если фронтенд ОЧЕНЬ хочет эти поля, задаем им дефолты, чтобы не было ошибки
-    risk_level: str = "low"
+    emotion: float = 0.0
+    tonality: float = 0.0
+    relevance: float = 0.0
+    sentiment_label: Optional[str] = None
+    
+    # ИСПОЛЬЗУЕМ Field с default, чтобы Pydantic не искал это в объекте БД
+    category: Optional[str] = Field(default="General")
+    risk_level: Optional[str] = Field(default="low")
