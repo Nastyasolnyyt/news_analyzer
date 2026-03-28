615650379096581bbc12cfed3e06725eda91c5df
Nastyasolnyyt <nastyasolntsevasunny@gmail.com>
2026-03-28 19:15:44 +0300

    fixed risklevel

--- services/risklevel-classifier/app/main.py
+++ services/risklevel-classifier/app/main.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.dialects.postgresql import insert
 
 # Импортируем наш классификатор
-from classifier_hf import HFRiskClassifier
+from classifier import HFRiskClassifier
 
 # Настройка логирования
 logging.basicConfig(
