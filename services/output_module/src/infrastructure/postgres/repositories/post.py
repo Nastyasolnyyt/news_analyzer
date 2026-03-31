bfaa8c05c870776ea5734f22bee0fc983476ccb3
Nastyasolnyyt <nastyasolntsevasunny@gmail.com>
2026-03-31 17:42:18 +0300

    in models post.py

--- services/output_module/src/infrastructure/postgres/repositories/post.py
+++ services/output_module/src/infrastructure/postgres/repositories/post.py
@@ -95,7 +95,7 @@ class PostDBGateWay:
         response_items = []
         for article in articles:
             analysis = article.analyses[0] if article.analyses else None
-            risk = relationship("Risk", back_populates="article", uselist=False)
+            risk = article.risks[0] if article.risks else None
             
             response_items.append({
                 "post": article,
